0fd0ec898cc53f70abdf407fa91348b2dc8df672
mattc489 <machan12@s.sfusd.edu>
2025-04-10 23:36:18 -0700

    Update Sound.java

--- src/main/java/Sound.java
+++ src/main/java/Sound.java
@@ -13,25 +13,22 @@ public class Sound
    *         Precondition: limit >= 0
    *  @return the number of values in this sound that this method changed
    */
-  public int limitAmplitude(int limit)
-{
+  public int limitAmplitude(int limit) {
   int count = 0;
   for (int i = 0; i < samples.length; i++) {
     if (samples[i] > limit) {
       samples[i] = limit;
       count++;
-    } 
+    } else if (samples[i] < -limit) {
       samples[i] = -limit;
       count++;
     }
   }
   return count;
 }
-
-public void trimSilenceFromBeginning()
-{
+public void trimSilenceFromBeginning() {
   int n = 0;
-  while (samples[n] == 0) {
+  while (n < samples.length && samples[n] == 0) {
     n++;
   }
   int[] newSamples = new int[samples.length - n];
